89b2346f66574bb11adaec33e0eb10a66f05071c
WallaceOliveiradosSantos <wallace@docente.senai.br>
2026-08-06 20:39:43 -0300

    Criado atributos para alterar, Criado as funções de para carregar os dados no formulário, alterar

--- src/app/component/formulario/formulario.ts
+++ src/app/component/formulario/formulario.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { PessoaService } from '../../services/pessoa-service';
 import { Pessoa } from '../../models/pessoa';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -15,22 +16,10 @@ export class Formulario {
   email = ''
   cpf = ''
   dataNascimento = ''
+  idPessoaEdit = 0
+  edit = false
 
-  constructor(private pessoaService: PessoaService) { }
-
-  save() {
-    
-    this.pessoaService.adicionar({
-      id: this.pessoaService.tamanhoArray() +1,
-      nome: this.nome,
-      cpf: this.cpf,
-      email: this.email,
-      dataNascimento: this.dataNascimento
-
-    })
-
-    this.limpaAtributos()
-  }
+  constructor(private route: ActivatedRoute, private pessoaService: PessoaService) { }
 
   limpaAtributos() {
     this.nome = ''
@@ -39,5 +28,66 @@ export class Formulario {
     this.dataNascimento = ''
   }
 
+  carregaAtributos(pessoa: Pessoa) {
+    this.nome = String(pessoa.nome)
+    this.email = String(pessoa.email)
+    this.cpf = String(pessoa.cpf)
+    this.dataNascimento = String(pessoa.dataNascimento)
+  }
+
+  ngOnInit() {
+    const idPessoa = this.route.snapshot.paramMap.get('id')
+
+    this.idPessoaEdit = Number(idPessoa)
+
+    if (idPessoa) {
+      this.edit = true
+
+      this.pessoaService.buscarPorId(Number(idPessoa))
+        .subscribe(objPessoa => {
+          if (objPessoa) {
+            this.carregaAtributos({ ...objPessoa })
+          }
+        })
+    }
+
+  }
+
+  save() {
+
+    const pessoa = new Pessoa()
+    pessoa.nome = this.nome
+    pessoa.email = this.email
+    pessoa.cpf = this.cpf
+    pessoa.dataNascimento = this.dataNascimento
+
+    if (this.edit) {
+      pessoa.id = this.idPessoaEdit
+      this.pessoaService.editar(pessoa)
+      this.edit = false
+    } else {
+      pessoa.id = this.pessoaService.tamanhoArray() + 1, //ARMENGUE PARA GERAR ID
+
+        this.pessoaService.adicionar(
+          pessoa
+          /*{
+          id: this.pessoaService.tamanhoArray() + 1, //ARMENGUE PARA GERAR ID
+          nome: this.nome,
+          cpf: this.cpf,
+          email: this.email,
+          dataNascimento: this.dataNascimento
+        }*/
+
+        )
+    }
+
+    this.limpaAtributos()
+  }
+
+  alterar(pessoa: Pessoa) {
+    if (confirm("Tem certeza que deseja Excluir a Pessoa?")) {
+      this.pessoaService.editar(pessoa)
+    }
+  }
 
 }
