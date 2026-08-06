6ccaafe6c558aaa3c2c4cefb8b04233cc29d7b15
WallaceOliveiradosSantos <wallace@docente.senai.br>
2026-08-06 20:43:35 -0300

    Criado a função para contar array, criado as funções para: Buscar um elemento por id, editar, e exluir

--- src/app/services/pessoa-service.ts
+++ src/app/services/pessoa-service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Pessoa } from '../models/pessoa';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PessoaService {
   private pessoas: Pessoa[] = []
+
+  tamanhoArray() {
+    return this.pessoas.length
+  }
 
   adicionar(pessoa: Pessoa) {
     this.pessoas.push(pessoa)
@@ -15,8 +20,23 @@ export class PessoaService {
     return this.pessoas
   }
 
-  tamanhoArray(){
-    return this.pessoas.length
+  buscarPorId(id: number) {
+    const pessoa = this.pessoas.find(elem => elem.id == id)
+    
+    return of(pessoa)
   }
+
+  editar(pessoa: Pessoa) {
+    const posArray = this.pessoas.findIndex(elem => elem.id === pessoa.id)
+
+    if (posArray !== -1) {
+      this.pessoas[posArray] = pessoa
+    }
+  }
+
+  excluir(id: number) {
+    this.pessoas = this.pessoas.filter(elem => elem.id !== id)
+  }
+
 
 }
